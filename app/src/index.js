65a07e256490866b38f4d40969d2ed9ffd36687d
Kathleen Lesouple <kathleen.lesouple@crelan.be>
2016-03-08 18:53:10 +0100

    add routing

--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,26 @@
 (function () {
     'use strict';
 
-    angular.module('bookstore', []);
+    angular.module('bookstore', ['ngRoute'])
+        .config(routes);
+    document.addEventListener('DOMContentLoaded', function(){
+        angular.bootstrap(document, ['bookstore']);
+    })
+
+    function routes($routeProvider){
+        $routeProvider
+            .when('/', {
+                templateUrl: 'templates/books.html',
+                controller: 'BooksController'
+            })
+            .when('/books', {
+                templateUrl: 'templates/books.html',
+                controller: 'BooksController'
+            })
+            .when('/books/:id', {
+                templateUrl: 'templates/bookDetails.html',
+                controller: 'BooksDetailsController'
+            })
+    }
 })();
 
